4273c9865757a251e1df66d261d3ae21b9cf0399
DinhHoang47 <dinhhoang0407@gmail.com>
2025-03-17 07:35:41 +0700

    update layout

--- app/(with-header)/collections/[id]/components/MobileRow.jsx
+++ app/(with-header)/collections/[id]/components/MobileRow.jsx
@@ -93,80 +93,6 @@ export default function MobileRow({
     >
       {/* Layout: image, right content */}
       <div className="flex">
-        {/* Image */}
-        <div className="mr-2">
-          {/* Image */}
-          {displayImg &&
-            (cardData.image_url || selectedFileUrl || editting) && (
-              <div className={`relative flex items-center`}>
-                <div className={`relative w-32 h-32`}>
-                  {selectedFileUrl && editting && (
-                    <Image
-                      priority={false}
-                      fill
-                      sizes="128px"
-                      alt="card-image"
-                      className="object-contain"
-                      src={selectedFileUrl}
-                    />
-                  )}
-                  {cardData.image_url && !selectedFileUrl && (
-                    <Image
-                      priority={false}
-                      fill
-                      sizes="128px"
-                      alt="card-image"
-                      className="object-contain"
-                      src={cardData.image_url}
-                    />
-                  )}
-                  {!cardData.image_url && !selectedFileUrl && (
-                    <Image
-                      priority={false}
-                      fill
-                      sizes="128px"
-                      alt="card-image"
-                      className="object-contain"
-                      src={"/assets/images/uni-placeholder-image.png"}
-                    />
-                  )}
-                  {loadingImage && (
-                    <label className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-                      <Spinner className="animate-spin h-5 w-5 text-blue-700" />
-                    </label>
-                  )}
-                </div>
-                {editting && (
-                  <>
-                    <input
-                      hidden
-                      id={cardData.id}
-                      onChange={(e) => {
-                        handleSelectedImage({
-                          selectedFile: e.target.files[0],
-                          inputTarget: e.target.value,
-                          setErrMsg,
-                          setSelectedFile,
-                          setSelectedFileUrl,
-                          setLoadingImage,
-                        });
-                      }}
-                      multiple={false}
-                      className="ml-4"
-                      accept="image/*"
-                      type="file"
-                    />
-                    <label
-                      className="obsolute cursor-pointer"
-                      htmlFor={cardData.id}
-                    >
-                      <DocumentArrowUpIcon className="h-6 w-6 text-blue-600" />
-                    </label>
-                  </>
-                )}
-              </div>
-            )}
-        </div>
         {/* Content */}
         <div className="flex-1 flex flex-col justify-between">
           {/* Term */}
@@ -186,18 +112,24 @@ export default function MobileRow({
             {!editting && (
               <>
                 <div className="flex flex-wrap justify-between w-full mr-2">
-                  <span className="font-semibold text-2xl flex-auto min-w-0">
-                    {cardData.term}
-                  </span>
-                  <span>{cardData.definition_2}</span>
-                </div>
-                {languageRef && (
-                  <div className="">
-                    <TextToSpeech text={cardData.term} lang={languageRef}>
-                      <SpeakerWaveIcon className="h-5 w-5 text-gray-500 hover:text-blue-500 cursor-pointer" />
-                    </TextToSpeech>
+                  <div className="flex items-center flex-auto min-w-0">
+                    <span className="font-semibold text-2xl ">
+                      {cardData.term}
+                    </span>
+                    <div className="ml-3">
+                      {languageRef && (
+                        <div className="">
+                          <TextToSpeech text={cardData.term} lang={languageRef}>
+                            <SpeakerWaveIcon className="h-7 w-7 text-gray-500 hover:text-blue-500 cursor-pointer" />
+                          </TextToSpeech>
+                        </div>
+                      )}
+                    </div>
+                    <span className="ml-5 text-lg">
+                      {cardData.definition_2}
+                    </span>
                   </div>
-                )}
+                </div>
               </>
             )}
           </div>
@@ -273,6 +205,80 @@ export default function MobileRow({
             )}
           </div>
         </div>
+        {/* Image */}
+        <div className="mr-2">
+          {/* Image */}
+          {displayImg &&
+            (cardData.image_url || selectedFileUrl || editting) && (
+              <div className={`relative flex items-center`}>
+                <div className={`relative w-24 h-24`}>
+                  {selectedFileUrl && editting && (
+                    <Image
+                      priority={false}
+                      fill
+                      sizes="96px"
+                      alt="card-image"
+                      className="object-contain rounded-md"
+                      src={selectedFileUrl}
+                    />
+                  )}
+                  {cardData.image_url && !selectedFileUrl && (
+                    <Image
+                      priority={false}
+                      fill
+                      sizes="96px"
+                      alt="card-image"
+                      className="object-contain rounded-md"
+                      src={cardData.image_url}
+                    />
+                  )}
+                  {!cardData.image_url && !selectedFileUrl && (
+                    <Image
+                      priority={false}
+                      fill
+                      sizes="128px"
+                      alt="card-image"
+                      className="object-contain"
+                      src={"/assets/images/uni-placeholder-image.png"}
+                    />
+                  )}
+                  {loadingImage && (
+                    <label className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                      <Spinner className="animate-spin h-5 w-5 text-blue-700" />
+                    </label>
+                  )}
+                </div>
+                {editting && (
+                  <>
+                    <input
+                      hidden
+                      id={cardData.id}
+                      onChange={(e) => {
+                        handleSelectedImage({
+                          selectedFile: e.target.files[0],
+                          inputTarget: e.target.value,
+                          setErrMsg,
+                          setSelectedFile,
+                          setSelectedFileUrl,
+                          setLoadingImage,
+                        });
+                      }}
+                      multiple={false}
+                      className="ml-4"
+                      accept="image/*"
+                      type="file"
+                    />
+                    <label
+                      className="obsolute cursor-pointer"
+                      htmlFor={cardData.id}
+                    >
+                      <DocumentArrowUpIcon className="h-6 w-6 text-blue-600" />
+                    </label>
+                  </>
+                )}
+              </div>
+            )}
+        </div>
       </div>
 
       {/* Error message */}
@@ -284,13 +290,13 @@ export default function MobileRow({
 
       {/* Actions */}
       {isOwner && (
-        <div className="absolute top-full right-0 !mt-0 -translate-y-8  flex items-center space-x-4 mr-6">
+        <div className="absolute top-full right-0 !mt-0 -translate-y-8 -translate-x-[160px]  flex items-center space-x-4 mr-6">
           {!editting && (
             <>
               <button>
                 <PencilIcon
                   onClick={handleEdit}
-                  className="h-5 w-5 text-blue-500"
+                  className="h-6 w-6 text-blue-500"
                 />
               </button>
             </>
@@ -302,7 +308,7 @@ export default function MobileRow({
                   handleDelete(cardData.id);
                 }}
               >
-                <TrashIcon className="h-5 w-5 text-red-400" />
+                <TrashIcon className="h-6 w-6 text-red-400" />
               </button>
               <button
                 disabled={loadingImage}
